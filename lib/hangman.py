d7e648db400946810823e2e59d8e61f49774c955
Jennifer McDowell <218159393+JenMcDowell@users.noreply.github.com>
2026-07-22 16:38:56 +0100

    code for get_random_word and enter_name

--- lib/hangman.py
+++ lib/hangman.py
@@ -1,7 +1,9 @@
-class Hangman:
-    pass
+import random
 
-HANGMAN_PICS = ['''
+class Hangman:
+
+#global varibles
+    HANGMAN_PICS = ['''
   +---+
       |
       |
@@ -37,3 +39,28 @@ HANGMAN_PICS = ['''
  /|\\  |
  / \\  |
      ===''']
+
+word_bank = "aardvark alligator alpaca badger bear beaver cat canary cow donkey deer dog elephant elk eagle ferret fish flamingo giraffe goat goose hippopotamus hedgehog hamster iguana impala ibex jellyfish jaguar jackal kangaroo koala kookaburra lemur llama lion monkey meerkat mouse newt narwhal nighthawk otter owl octopus panda penguin pig quokka quail quetzal raccoon rabbit rhinoceros seal snake salmon tiger tortoise toucan unicornfish umbrellabird viper vulture    walrus wolverine wombat yak yellowjacket yellowhammer zebra zebu".split()     
+   
+
+#def __init__(self, [word_bank], secret_word = None):
+    
+
+# This function will return a random string from a list of strings.
+def get_random_word(wordList):
+    #this is going through word_bank one word at a time to end of list.
+    wordIndex = random.randint(0, len(wordList)-1) 
+    #this is returning the random word 
+    return wordList[wordIndex] 
+
+# This function will return entered user name with welcome message. 
+def enter_name():
+    name = input("what is your name?").strip()
+    if name == "": 
+        name = "Player"
+    print(f"Welcome to the game {name}!! 🤗")    
+    return name
+    
+    
+# This function will display the game board
+#def display_board(self):
